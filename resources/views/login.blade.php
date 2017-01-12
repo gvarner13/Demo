f8e8a4f8990594825fee70c819007ebf539eeb97
= <=>
2017-01-12 19:49:02 +0000

    Fisrt commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+  <div class='login'>
+    <div class='login_title'>
+      <span>Login to the demo</span>
+    </div>
+    <div class='login_fields'>
+      <div class='login_fields__user'>
+        <div class='icon'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/user_icon_copy.png'>
+        </div>
+        <input placeholder='Username' type='text'>
+          <div class='validation'>
+            <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+          </div>
+        </input>
+      </div>
+      <div class='login_fields__password'>
+        <div class='icon'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/lock_icon_copy.png'>
+        </div>
+        <input placeholder='Password' type='password'>
+        <div class='validation'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+        </div>
+      </div>
+      <div class='login_fields__submit'>
+        <input type='submit' value='Log In'>
+        <div class='forgot'>
+          <a href='#'>Forgot password?</a>
+        </div>
+      </div>
+    </div>
+    <div class='disclaimer'>
+      <p>This is the demo site for my projects and clients. If you have trouble with login please contact me direct.</p>
+    </div>
+  </div>
+
+</body>
+
+</html>
